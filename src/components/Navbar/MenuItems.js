5645365ab076101133ade48d6e5c1f4bb44503ab
Валуйских Никита <73479799+Cry0frog@users.noreply.github.com>
2021-01-20 20:01:31 +0300

    #navbar
    первая попытка создать навигационный бар

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: 'home',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Services',
+        url: 'services',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Products',
+        url: 'products',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact us',
+        url: 'contact-us',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: 'sign-up',
+        cName: 'nav-links-mobile'
+    }
+]
